df87959eaea4f36d65119ee94a74d47ef7514a4c
Sillysally <92034603+SallyResch@users.noreply.github.com>
2025-02-28 11:34:31 +0100

    made page more responsive

--- app/page.js
+++ app/page.js
@@ -3,6 +3,34 @@ import styles from "./page.module.css";
 import gitImage from "../public/github.png";
 import linkedImage from "../public/linkedin.png";
 import emailImage from "../public/email.png";
+import ProjectList from "../components/ProjectList";
+
+const workProjects = [
+  { name: "HEYEY", url: "https://www.heyey.se" },
+  { name: "Elevateyourself AB", url: "https://www.elevateyourself.se" },
+];
+
+const selfMadeProjects = [
+  { name: "AccessibilityQuiz", url: "/htmlCss/AccessibilityQuizBuilding/index.html" },
+  { name: "BalanceSheet", url: "/htmlCss/BalanceSheet/index.html" },
+  { name: "CafeMenuPage", url: "/htmlCss/CafeMenuApp/index.html" },
+  { name: "CatPhotoPage", url: "/htmlCss/CatPhotoApp/index.html" },
+  { name: "ColorMarkers", url: "/htmlCss/ColorMarkersApp/index.html" },
+  { name: "CssBoxModelRothko", url: "/htmlCss/CssBoxModelRothko/index.html" },
+  { name: "CssCat", url: "/htmlCss/CssCat/index.html" },
+  { name: "FerrisWheel", url: "/htmlCss/FerrisWheel/index.html" },
+  { name: "Magazine", url: "/htmlCss/Magazine/index.html" },
+  { name: "NutritionLabel", url: "/htmlCss/NutritionLabel/index.html" },
+  { name: "Penguin", url: "/htmlCss/Penguin/index.html" },
+  { name: "Photogallery", url: "/htmlCss/Photogallery/index.html" },
+  { name: "ProductLandingPage", url: "/htmlCss/ProductLandingPage/index.html" },
+  { name: "RegistrationForm", url: "/htmlCss/RegistrationForm/index.html" },
+  { name: "ResponsivePiano", url: "/htmlCss/ResponsivePiano/index.html" },
+  { name: "SkylineCssVariables", url: "/htmlCss/SkylineCssVariables/index.html" },
+  { name: "SurveyForm", url: "/htmlCss/SurveyForm/index.html" },
+  { name: "TechnicalDoc", url: "/htmlCss/TechnicalDoc/index.html" },
+  { name: "TributePage", url: "/htmlCss/TributePage/index.html" },
+];
 
 
 export default function Home() {
@@ -28,102 +56,10 @@ export default function Home() {
           </div>
         </section>
         <section id="projects" className={styles.projects}>
-          <h1>Projects</h1>
-          <div className={styles.projectTile}>
-            <h3 className={styles.sectionText}>Work/Internship projects</h3>
-            <div className={styles.workProjects}>
-              <a className={styles.link} href="https://www.heyey.se" target="_blank" rel="noopener noreferrer">
-                <iframe className={styles.webFrame} src="https://www.heyey.se"  title="Iframe Example" scrolling="no"></iframe>
-                <p>HEYEY</p> 
-              </a>
-              <a className={styles.link} href="https://www.elevateyourself.se" target="_blank" rel="noopener noreferrer">
-                <iframe className={styles.webFrame} src="https://www.elevateyourself.se" title="Iframe Example" scrolling="no"></iframe>
-                <p>Elevateyourself AB</p> 
-              </a>
-            </div>
-          </div>
-         <div className={styles.projectTile}>
-          <h3 className={styles.sectionText}>Selfmade projects</h3>
-          <div className={styles.selfMadeProjects}>
-          <a className={styles.link} href="/htmlCss/AccessibilityQuizBuilding/index.html">
-          <iframe src="/htmlCss/AccessibilityQuizBuilding/index.html" className={styles.webFrame} title="Iframe Example" scrolling="no"></iframe>
-          <p>AccessibilityQuiz</p>
-          </a>
-          <a className={styles.link} href="/htmlCss/BalanceSheet/index.html">
-          <iframe src="/htmlCss/BalanceSheet/index.html" className={styles.webFrame} title="Iframe Example" scrolling="no"></iframe>
-          <p>BalanceSheet</p>
-          </a>
-          <a className={styles.link} href="/htmlCss/CafeMenuApp/index.html">
-          <iframe src="/htmlCss/CafeMenuApp/index.html" className={styles.webFrame} title="Iframe Example" scrolling="no"></iframe>
-          <p>CafeMenuPage</p>
-          </a>
-          <a className={styles.link} href="/htmlCss/CatPhotoApp/index.html">
-          <iframe src="/htmlCss/CatPhotoApp/index.html" className={styles.webFrame} title="Iframe Example" scrolling="no"></iframe>
-          <p>CatPhotoPage</p>
-          </a>
-          <a className={styles.link} href="/htmlCss/ColorMarkersApp/index.html">
-          <iframe src="/htmlCss/ColorMarkersApp/index.html" className={styles.webFrame} title="Iframe Example" scrolling="no"></iframe>
-          <p>ColorMarkers</p>
-          </a>
-          <a className={styles.link} href="/htmlCss/CssBoxModelRothko/index.html">
-          <iframe src="/htmlCss/CssBoxModelRothko/index.html" className={styles.webFrame} title="Iframe Example" scrolling="no"></iframe>
-          <p>CssBoxModelRothko</p>
-          </a>
-          <a className={styles.link} href="/htmlCss/CssCat/index.html">
-          <iframe src="/htmlCss/CssCat/index.html" className={styles.webFrame} title="Iframe Example" scrolling="no"></iframe>
-          <p>CssCat</p>
-          </a>
-          <a className={styles.link} href="/htmlCss/FerrisWheel/index.html">
-          <iframe src="/htmlCss/FerrisWheel/index.html" className={styles.webFrame} title="Iframe Example" scrolling="no"></iframe>
-          <p>FerrisWheel</p>
-          </a>
-          <a className={styles.link} href="/htmlCss/Magazine/index.html">
-          <iframe src="/htmlCss/Magazine/index.html" className={styles.webFrame} title="Iframe Example" scrolling="no"></iframe>
-          <p>Magazine</p>
-          </a>
-          <a className={styles.link} href="/htmlCss/NutritionLabel/index.html">
-          <iframe src="/htmlCss/NutritionLabel/index.html" className={styles.webFrame} title="Iframe Example" scrolling="no"></iframe>
-          <p>NutritionLabel</p>
-          </a>
-          <a className={styles.link} href="/htmlCss/Penguin/index.html">
-          <iframe src="/htmlCss/Penguin/index.html" className={styles.webFrame} title="Iframe Example" scrolling="no"></iframe>
-          <p>Penguin</p>
-          </a>
-          <a className={styles.link} href="/htmlCss/Photogallery/index.html">
-          <iframe src="/htmlCss/Photogallery/index.html" className={styles.webFrame} title="Iframe Example" scrolling="no"></iframe>
-          <p>Photogallery</p>
-          </a>
-          <a className={styles.link} href="/htmlCss/ProductLandingPage/index.html">
-          <iframe src="/htmlCss/ProductLandingPage/index.html" className={styles.webFrame} title="Iframe Example" scrolling="no"></iframe>
-          <p>ProductLandingPage</p>
-          </a>
-          <a className={styles.link} href="/htmlCss/RegistrationForm/index.html">
-          <iframe src="/htmlCss/RegistrationForm/index.html" className={styles.webFrame} title="Iframe Example" scrolling="no"></iframe>
-          <p>RegistrationForm</p>
-          </a>
-          <a className={styles.link} href="/htmlCss/ResponsivePiano/index.html">
-          <iframe src="/htmlCss/ResponsivePiano/index.html" className={styles.webFrame} title="Iframe Example" scrolling="no"></iframe>
-          <p>ResponsivePiano</p>
-          </a>
-          <a className={styles.link} href="/htmlCss/SkylineCssVariables/index.html">
-          <iframe src="/htmlCss/SkylineCssVariables/index.html" className={styles.webFrame} title="Iframe Example" scrolling="no"></iframe>
-          <p>SkylineCssVariables</p>
-          </a>
-          <a className={styles.link} href="/htmlCss/SurveyForm/index.html">
-          <iframe src="/htmlCss/SurveyForm/index.html" className={styles.webFrame} title="Iframe Example" scrolling="no"></iframe>
-          <p>SurveyForm</p>
-          </a>
-          <a className={styles.link} href="/htmlCss/TechnicalDoc/index.html">
-          <iframe src="/htmlCss/TechnicalDoc/index.html" className={styles.webFrame} title="Iframe Example" scrolling="no"></iframe>
-          <p>TechnicalDoc</p>
-          </a>
-          <a className={styles.link} href="/htmlCss/TributePage/index.html">
-          <iframe src="/htmlCss/TributePage/index.html" className={styles.webFrame} title="Iframe Example" scrolling="no"></iframe>
-          <p>TributePage</p>
-          </a>
-          </div>
-          </div>
-        </section> 
+        <h1>Projects</h1>
+        <ProjectList title="Work/Internship projects" projects={workProjects} />
+        <ProjectList title="Selfmade projects" projects={selfMadeProjects} />
+      </section>
       </main>
       <section className={styles.contact}> 
           <h1>Lets do projects together</h1>

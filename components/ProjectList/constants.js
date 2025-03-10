b1ec313fbd96dda56943b573e682092a01b09c2d
Sillysally <92034603+SallyResch@users.noreply.github.com>
2025-03-10 14:14:15 +0100

    added more descriptions

--- components/ProjectList/constants.js
+++ components/ProjectList/constants.js
@@ -8,12 +8,30 @@ export const workProjects = [
       url: "/htmlCss/AccessibilityQuizBuilding/index.html",
       description:"The Accessibility Quiz is an interactive and fully responsive web-based quiz designed to test users on HTML and CSS accessibility principles. The project follows best practices for accessibility (a11y), ensuring it is usable by screen readers and keyboard users."
     },
-    { name: "BalanceSheet",url: "/htmlCss/BalanceSheet/index.html", description:"The Balance Sheet project is a structured, accessible, and responsive financial statement that displays a company's Assets, Liabilities, and Net Worth over multiple years. This project provides a clear and interactive presentation of financial data using HTML and CSS, ensuring usability and accessibility for all users." },
-    { name: "CafeMenuPage", url: "/htmlCss/CafeMenuApp/index.html" },
-    { name: "CatPhotoPage", url: "/htmlCss/CatPhotoApp/index.html" },
-    { name: "ColorMarkers", url: "/htmlCss/ColorMarkersApp/index.html" },
-    { name: "CssBoxModelRothko", url: "/htmlCss/CssBoxModelRothko/index.html" },
-    { name: "CssCat", url: "/htmlCss/CssCat/index.html" },
+    { name: "BalanceSheet",
+      url: "/htmlCss/BalanceSheet/index.html", 
+      description:"The Balance Sheet project is a structured, accessible, and responsive financial statement that displays a company's Assets, Liabilities, and Net Worth over multiple years. This project provides a clear and interactive presentation of financial data using HTML and CSS, ensuring usability and accessibility for all users." 
+    },
+    { name: "CafeMenuPage", 
+      url: "/htmlCss/CafeMenuApp/index.html",
+      description:"The Cafe Menu Page is a responsive web-based project that displays a cafe's menu items using HTML and CSS. The project includes a variety of menu items, such as beverages, desserts and is designed to be accessible and user-friendly on all devices." 
+    },
+    { name: "CatPhotoPage", 
+      url: "/htmlCss/CatPhotoApp/index.html",
+      description:"The Cat Photo Page is a responsive web-based project that displays a collection of cat photos using HTML and CSS. The project includes a variety of cat photos and is designed to be accessible and user-friendly on all devices."
+    },
+    { name: "ColorMarkers", 
+      url: "/htmlCss/ColorMarkersApp/index.html",
+      description:"The Color Markers project is an interactive and responsive web-based project that displays color markers using HTML and CSS. The project includes a variety of color markers and is designed to be accessible and user-friendly on all devices."
+    },
+    { name: "CssBoxModelRothko", 
+      url: "/htmlCss/CssBoxModelRothko/index.html",
+      description:"The CSS Box Model Rothko project is a responsive web-based project that displays a collection of color blocks using HTML and CSS. The project is inspired by the artist Mark Rothko and is designed to be accessible and user-friendly on all devices."
+    },
+    { name: "CssCat", 
+      url: "/htmlCss/CssCat/index.html",
+      description:"The CSS Cat project is a responsive web-based project that displays a cat using HTML and CSS. The project includes a variety of cat images and is designed to be accessible and user-friendly on all devices."
+    },
     { name: "FerrisWheel", url: "/htmlCss/FerrisWheel/index.html" },
     { name: "Magazine", url: "/htmlCss/Magazine/index.html" },
     { name: "NutritionLabel", url: "/htmlCss/NutritionLabel/index.html" },

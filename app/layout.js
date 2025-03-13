9b64d4362298d3c1f340ab64596e4396177d5399
Sillysally <92034603+SallyResch@users.noreply.github.com>
2025-03-13 13:30:13 +0100

    added vortex from Acernety

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import Navbar from "@/components/Navbar/Navbar";
-import Loading from "./loading.js";
+import Loading from "./loading";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
